e619f523ba2267d3f374222a3393ddbe401131a1
Leon <yurileon@hotmail.com.br>
2023-12-14 12:30:27 -0300

    contact hotfix

--- src/app/pc/page.tsx
+++ src/app/pc/page.tsx
@@ -1,19 +1,15 @@
 "use client";
-import ContactForm from "@/components/ContactForm";
-import ContactScene from "@/components/ContactScene";
 import FireRedText from "@/components/FireRedText";
 import { playSound } from "@/utils/playSound";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
 export default function PC() {
-  const [formState, setFormState] = useState<string>("idling");
   return (
     <main className="flex h-full min-h-screen flex-col items-center bg-white bg-[url('/assets/contact-bg2.png')] font-firered text-3xl text-neutral-700 justify-between">
       <span className="bg-blue-500 w-full h-16 flex justify-center items-center">
         <span className="container select-none px-3 flex w-full justify-between">
           <h1 className="text-white">
-            <FireRedText>Get in touch with Leon</FireRedText>
+            <FireRedText>Leon's Features</FireRedText>
           </h1>
           <Link
             onMouseEnter={() => playSound("/assets/sounds/btn.wav")}
@@ -32,10 +28,7 @@ export default function PC() {
           </Link>
         </span>
       </span>
-      <div className="container flex-1 mb-6 h-full md:gap-12 md:my-6 px-3 flex flex-col bg-white/50 shadow-[inset_0px_0px_30px_0px_rgba(250,237,176,0.7)] rounded-[40px] md:flex-row mx-auto md:p-3 items-center justify-center">
-        <ContactScene width={200} formState={formState} />
-        <ContactForm setFormState={setFormState} />
-      </div>
+      <div className="container flex-1 mb-6 h-full md:gap-12 md:my-6 px-3 flex flex-col bg-white/50 shadow-[inset_0px_0px_30px_0px_rgba(250,237,176,0.7)] rounded-[40px] md:flex-row mx-auto md:p-3 items-center justify-center"></div>
     </main>
   );
 }
---
+++ src/utils/features.js
@@ -0,0 +1,14 @@
+export const features = [
+  {
+    name: "Hubbi",
+    briefing: "Hello world",
+    url: "http://hubbi.app",
+    images: [],
+  },
+  {
+    name: "Gamemind",
+    briefing: "Hellow ord",
+    url: "https://gmnd.vercel.app",
+    images: [],
+  },
+];
