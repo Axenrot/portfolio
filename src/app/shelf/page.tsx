2964554c4626284edb66025a069e67baa26b127b
Leon <yurileon@hotmail.com.br>
2023-12-21 10:54:22 -0300

    contact page responsivity

--- src/app/shelf/page.tsx
+++ src/app/shelf/page.tsx
@@ -33,7 +33,7 @@ export default function Shelf() {
         </span>
       </span>
       <div className="container flex-1 mb-6 h-full md:gap-12 md:my-6 px-3 flex flex-col bg-white/50 shadow-[inset_0px_0px_30px_0px_rgba(250,237,176,0.7)] rounded-[40px] md:flex-row mx-auto md:p-3 items-center justify-center">
-        <ContactScene width={200} formState={formState} />
+        <ContactScene formState={formState} />
         <ContactForm setFormState={setFormState} />
       </div>
     </main>
